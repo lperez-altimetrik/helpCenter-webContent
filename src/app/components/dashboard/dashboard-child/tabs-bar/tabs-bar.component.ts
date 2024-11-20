92b8deddc51cc00877bb89ba7a616b5b0d0f3743
Alejandro Rangel <alejandrorangel@ITadmins-MacBook-Pro.local>
2024-11-20 12:31:46 -0600

    moving search to shared and adding tabs-bar component to dashboard-child

---
+++ src/app/components/dashboard/dashboard-child/tabs-bar/tabs-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './tabs-bar.component.html',
+  styleUrl: './tabs-bar.component.scss'
+})
+export class TabsBarComponent {
+
+}
