9b96f8982797c7ec5acc93bee1ae92dfd9da3aea
gsuarez17 <gasuarez@altimetrik.com>
2024-11-22 20:52:52 -0600

    component cleanup for web versions of top-bar and footer. Made 'Inter' the default angular materials font-family.

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { inject } from '@angular/core';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatDividerModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
