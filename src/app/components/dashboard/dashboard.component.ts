3f55664b6108d2f5ad7cc7d29ee0e148d1c8a141
acamachoaltimetrik <acamacho@altimetrik.com>
2024-11-26 16:38:20 -0400

    [HSPS] - contact us component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,30 +3,14 @@ import { Component } from '@angular/core';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ContactUsComponent } from '../shared/contact-us/contact-us.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  template: `
-    <div>
-      <h2>Dashboard</h2>
-      <p>Welcome to your dashboard!</p>
-      <button (click)="logout()">Logout</button>
-    </div>
-  `,
-  styles: [
-    `
-      div {
-        text-align: center;
-        padding: 20px;
-      }
-      button {
-        margin-top: 20px;
-        padding: 10px 20px;
-        font-size: 16px;
-      }
-    `,
-  ],
+  imports: [ContactUsComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   private authService = inject(AuthService);
