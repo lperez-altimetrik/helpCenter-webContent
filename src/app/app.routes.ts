41d670de2bfe2cadf663dbeaade36824bf8ca116
gsuarez17 <gasuarez@altimetrik.com>
2024-12-03 13:26:05 -0600

    removed testing routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { authGuard } from './guards/auth.guard';
-import { TopBarComponent } from './components/shared/top-bar/top-bar.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'top-bar-test', component: TopBarComponent},
-  { path: 'footer-test', component: FooterComponent},
   {
     path: 'dashboard',
     component: DashboardComponent,
