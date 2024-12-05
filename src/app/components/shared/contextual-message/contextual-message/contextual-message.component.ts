d7608f4e1116e5f05cf127050b62e90daa86f33c
gsuarez17 <gasuarez@altimetrik.com>
2024-12-05 15:30:56 -0600

    Contextual message style switcher implemented.

--- src/app/components/shared/contextual-message/contextual-message/contextual-message.component.ts
+++ src/app/components/shared/contextual-message/contextual-message/contextual-message.component.ts
@@ -1,6 +1,7 @@
 import { NgClass } from '@angular/common';
 import { Component,Input } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { of } from 'rxjs';
 
 
 export enum MessageType {
@@ -18,28 +19,31 @@ export enum MessageType {
   styleUrl: './contextual-message.component.scss'
 })
 export class ContextualMessageComponent {
+  readonly DETAIL_STYLE_STRING: string = "-detail";
+  readonly TITLE_STYLE_STRING: string = "-title";
   @Input() title: string = '';
-  @Input() content: string = '';
+  @Input() message: string = '';
   @Input() messageType: string = '';
+  
 
   private _messageValue: MessageType = MessageType.Neutral;
 
-  constructor() {
-    this.title = 'Title Text';
-    this.content= 'Little red riding hood went on a mission accross the sea to find someone with whom to share her wheat. The big bad wolf followed in his submarine to see if he could make her into a meal to eat.';
-  }
-
+  //'Danger','Warning','Neutral' and 'Positive' are accepted inputs for State.
   @Input() set state(value: string) {
     const state = MessageType[value as keyof typeof MessageType];
     this._messageValue = state || MessageType.Neutral;
   }
 
   get detailTypeClass(): string {
-    return this._messageValue + "-detail";
+    return this._messageValue + this.DETAIL_STYLE_STRING;
   }
 
   get titleTypeClass(): string {
-    return this._messageValue + "-title";
+    return this._messageValue + this.TITLE_STYLE_STRING;
   }
 
 }
+
+/*
+Little Red Riding Hood went on a mission accross the sea to find someone with whom to share her wheat. The Big Bad Wolf followed in his submarine to see if he could make her into a meal to eat.Little Red Riding Hood went on a mission accross the sea to find someone with whom to share her wheat. The Big Bad Wolf followed in his submarine to see if he could make her into a meal to eat. Little Red Riding Hood went on a mission accross the sea to find someone with whom to share her wheat. The Big Bad Wolf followed in his submarine to see if he could make her into a meal to eat.Little Red Riding Hood went on a mission accross the sea to find someone with whom to share her wheat. The Big Bad Wolf followed in his submarine to see if he could make her into a meal to eat. Little Red Riding Hood went on a mission accross the sea to find someone with whom to share her wheat. The Big Bad Wolf followed in his submarine to see if he could make her into a meal to eat.Little Red Riding Hood went on a mission accross the sea to find someone with whom to share her wheat. The Big Bad Wolf followed in his submarine to see if he could make her into a meal to eat. Little Red Riding Hood went on a mission accross the sea to find someone with whom to share her wheat. The Big Bad Wolf followed in his submarine to see if he could make her into a meal to eat.Little Red Riding Hood went on a mission accross the sea to find someone with whom to share her wheat. The Big Bad Wolf followed in his submarine to see if he could make her into a meal to eat.
+*/
---
+++ src/stories/contextual-message.component.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, moduleMetadata, StoryFn } from '@storybook/angular';
+import { ContextualMessageComponent } from 'app/components/shared/contextual-message/contextual-message/contextual-message.component';
+import { MatIconModule } from '@angular/material/icon';
+export default {
+  title: 'Shared/Contextual-Message',
+  component: ContextualMessageComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [MatIconModule],
+    }),
+  ],
+} as Meta;
+
+const Template: StoryFn = (args) => ({
+  component: ContextualMessageComponent,
+  props: args,
+});
+
+export const Default = Template.bind({});
+Default.args = {};
