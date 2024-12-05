532964f16ea2d3b4a6b0170e005665233eabab1d
Alejandro Rangel <alejandrorangel@ITadmins-MacBook-Pro.local>
2024-12-04 18:02:19 -0600

    updating components for fixing typos and adding new behavior

---
+++ src/app/components/shared/breadcrum/breadcrumb.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-breadcrum',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './breadcrumb.component.html',
+  styleUrl: './breadcrumb.component.scss'
+})
+export class BreadcrumbComponent {
+  public pagesPath: any = [
+    { title: "Page 1", url: "#"},
+    { title: "Page 2", url: "#"},
+    { title: "Page 3", url: "#"},
+    { title: "Page 4", url: "#"}
+  ]
+  public currentElement: string = "page 5";
+}
