f2cd98a8bb5c61694e40229edd847bd1df2b1078
acamachoaltimetrik <acamacho@altimetrik.com>
2024-12-05 15:01:52 -0400

    added to storybook

--- src/app/components/shared/accordion/accordion.component.ts
+++ src/app/components/shared/accordion/accordion.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { LinkComponent } from '../link/link.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-accordion',
   standalone: true,
-  imports: [CommonModule, MatExpansionModule, LinkComponent],
+  imports: [MatExpansionModule, LinkComponent],
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss',
 })
