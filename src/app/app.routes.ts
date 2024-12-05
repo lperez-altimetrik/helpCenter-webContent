ac1c147e6b590563cce74b9a530547c1937d2dce
gsuarez17 <gasuarez@altimetrik.com>
2024-12-05 15:47:27 -0600

    removed test route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { authGuard } from './guards/auth.guard';
-import { ContextualMessageComponent } from './components/shared/contextual-message/contextual-message/contextual-message.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'context', component:ContextualMessageComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
