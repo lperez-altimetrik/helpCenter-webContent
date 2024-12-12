aee48cc9d20a324b1feee95e636b1fd0fdde064e
gsuarez17 <gasuarez@altimetrik.com>
2024-12-12 10:32:00 -0600

    Most of design in order.

--- src/app/components/shared/modal/modal.component.ts
+++ src/app/components/shared/modal/modal.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
