685bab308e1ba5946714d50b51ec91da86556aae
Alejandro Rangel <alejandrorangel@ITadmins-MacBook-Pro.local>
2024-12-16 13:41:24 -0600

    removing unused imports

--- src/app/components/shared/search-bar/search-bar.component.ts
+++ src/app/components/shared/search-bar/search-bar.component.ts
@@ -1,8 +1,5 @@
 import {Component, Input} from '@angular/core';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import {AsyncPipe} from '@angular/common';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -15,7 +12,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatInputModule,
     MatAutocompleteModule,
     ReactiveFormsModule,
-    AsyncPipe,],
+    ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
