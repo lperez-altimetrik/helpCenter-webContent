f84e8f9fee0c87563350aaaff901bb0cf041ec42
gsuarez17 <gasuarez@altimetrik.com>
2025-01-22 12:14:38 -0600

    updates modal functionality to close when the X button is clicked and to overlay the modal as intended.

--- src/app/components/shared/modal/modal.component.ts
+++ src/app/components/shared/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,7 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
 })
@@ -17,4 +18,10 @@ export class ModalComponent {
     'We give ambitious businesses their launchpad through safe and secure online payment solutionsWe give ambitious businesses their launchpad through safe and secure online payment solutions';
   @Input() primaryButton = 'Send';
   @Input() secondaryButton = 'No, Thanks';
+
+  showModal = true;
+
+  closeModal() {
+    this.showModal = false;
+  }
 }
